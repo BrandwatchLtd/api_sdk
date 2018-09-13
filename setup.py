050eefd8cc70b7c7959a394c65b5745e78eeb001
Hamish Morgan <hamish.morgan@gmail.com>
2018-09-13 13:23:41 +0100

    Moved to python 3.7

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
 
     py_modules=['bwproject', 'bwresources', 'bwdata', 'filters'],
