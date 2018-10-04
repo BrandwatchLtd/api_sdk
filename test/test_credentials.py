d153a92bd3f7b2fb8f7e30108c3e262a8ee1f585
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-04 10:07:15 +0100

    Updated credential store tests to use a context manager

--- test/test_credentials.py
+++ test/test_credentials.py
@@ -10,91 +10,101 @@ ACCESS_TOKEN = "00000000-0000-0000-0000-000000000000"
 
 class TestCredentialsStore(unittest.TestCase):
 
-    def setUp(self):
-        self.credentials_dir = Path(tempfile.NamedTemporaryFile().name)
-        self.credentials_file = self.credentials_dir / 'tokens.txt'
-        self.credentials_store = CredentialsStore(credentials_path=self.credentials_file)
+    def with_credential_store(function):
+        def wrapper(self):
+            with tempfile.TemporaryDirectory() as temp_dir:
+                token_path = Path(temp_dir) / 'tokens.txt'
+                store = CredentialsStore(credentials_path=token_path)
+                function(self, store)
+        return wrapper
 
-    def tearDown(self):
-        if self.credentials_file.exists():
-            self.credentials_file.unlink()
-        if self.credentials_dir.exists():
-            self.credentials_dir.rmdir()
+    @with_credential_store
+    def test_file_created_on_read(self, store):
+        self.assertFalse(store._credentials_path.exists())
 
-    def test_file_created_on_read(self):
-        self.assertFalse(self.credentials_file.exists())
+        _ = [c for c in store]
 
-        _ = [c for c in self.credentials_store]
+        self.assertTrue(store._credentials_path.exists())
 
-        self.assertTrue(self.credentials_file.exists())
+    @with_credential_store
+    def test_file_created_on_write(self, store):
+        self.assertFalse(store._credentials_path.exists())
 
-    def test_file_created_on_write(self):
-        self.assertFalse(self.credentials_file.exists())
+        store["example@example.com"] = ACCESS_TOKEN
 
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
+        self.assertTrue(store._credentials_path.exists())
 
-        self.assertTrue(self.credentials_file.exists())
+    @with_credential_store
+    def test_store(self, store):
+        self.assertEqual(len(store), 0)
 
-    def test_store(self):
-        self.assertEqual(len(self.credentials_store), 0)
+        store["example@example.com"] = ACCESS_TOKEN
 
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
+        self.assertEqual(len(store), 1)
 
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
-        self.assertEqual(len(self.credentials_store), 1)
+    @with_credential_store
+    def test_store_multiple(self, store):
+        self.assertEqual(len(store), 0)
 
-    def test_store_multiple(self):
-        self.assertEqual(len(self.credentials_store), 0)
+        store["example@example.com"] = "10000000-0000-0000-0000-000000000000"
+        store["another-example@example.com"] = "20000000-0000-0000-0000-000000000000"
 
-        self.credentials_store["example@example.com"] = "10000000-0000-0000-0000-000000000000"
-        self.credentials_store["another-example@example.com"] = "20000000-0000-0000-0000-000000000000"
+        self.assertEqual(store["example@example.com"], "10000000-0000-0000-0000-000000000000")
+        self.assertEqual(store["another-example@example.com"], "20000000-0000-0000-0000-000000000000")
+        self.assertEqual(len(store), 2)
 
-        self.assertEqual(self.credentials_store["example@example.com"], "10000000-0000-0000-0000-000000000000")
-        self.assertEqual(self.credentials_store["another-example@example.com"], "20000000-0000-0000-0000-000000000000")
-        self.assertEqual(len(self.credentials_store), 2)
+    @with_credential_store
+    def test_store_overwrite(self, store):
+        self.assertEqual(len(store), 0)
 
-    def test_store_overwrite(self):
-        self.assertEqual(len(self.credentials_store), 0)
+        store["example@example.com"] = "10000000-0000-0000-0000-000000000000"
+        store["example@example.com"] = "20000000-0000-0000-0000-000000000000"
 
-        self.credentials_store["example@example.com"] = "10000000-0000-0000-0000-000000000000"
-        self.credentials_store["example@example.com"] = "20000000-0000-0000-0000-000000000000"
+        self.assertEqual(store["example@example.com"], "20000000-0000-0000-0000-000000000000")
 
-        self.assertEqual(self.credentials_store["example@example.com"], "20000000-0000-0000-0000-000000000000")
+    @with_credential_store
+    def test_store_same(self, store):
+        self.assertEqual(len(store), 0)
 
-    def test_store_same(self):
-        self.assertEqual(len(self.credentials_store), 0)
+        store["example@example.com"] = ACCESS_TOKEN
+        store["example@example.com"] = ACCESS_TOKEN
 
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
 
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_store_case_insensitive(self, store):
+        store["example@example.com"] = ACCESS_TOKEN
+        store["EXAMPLE@EXAMPLE.COM"] = ACCESS_TOKEN
+        store["eXaMpLe@ExAmPlE.cOm"] = ACCESS_TOKEN
+        self.assertEqual(len(store), 1)
 
-    def test_store_case_insensitive(self):
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.credentials_store["EXAMPLE@EXAMPLE.COM"] = ACCESS_TOKEN
-        self.credentials_store["eXaMpLe@ExAmPlE.cOm"] = ACCESS_TOKEN
-        self.assertEqual(len(self.credentials_store), 1)
+    @with_credential_store
+    def test_store_lower(self, store):
+        store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
 
-    def test_store_lower(self):
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_store_upper(self, store):
+        store["EXAMPLE@EXAMPLE.COM"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
 
-    def test_store_upper(self):
-        self.credentials_store["EXAMPLE@EXAMPLE.COM"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_store_mixed(self, store):
+        store["eXaMpLe@ExAmPlE.cOm"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
 
-    def test_store_mixed(self):
-        self.credentials_store["eXaMpLe@ExAmPlE.cOm"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_get_lower(self, store):
+        store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["example@example.com"], ACCESS_TOKEN)
 
-    def test_get_lower(self):
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["example@example.com"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_get_upper(self, store):
+        store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["EXAMPLE@EXAMPLE.COM"], ACCESS_TOKEN)
 
-    def test_get_upper(self):
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["EXAMPLE@EXAMPLE.COM"], ACCESS_TOKEN)
-
-    def test_get_mixed(self):
-        self.credentials_store["example@example.com"] = ACCESS_TOKEN
-        self.assertEqual(self.credentials_store["eXaMpLe@ExAmPlE.cOm"], ACCESS_TOKEN)
+    @with_credential_store
+    def test_get_mixed(self, store):
+        store["example@example.com"] = ACCESS_TOKEN
+        self.assertEqual(store["eXaMpLe@ExAmPlE.cOm"], ACCESS_TOKEN)
