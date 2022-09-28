50c607d0870a230c89098adb27ffc650c35da75d
Joe Baines-Holmes <joebh@brandwatch.com>
2022-09-28 11:31:17 +0100

    Issue deprecation warning on package import
    This will let users know the package is deprecated when they import it.

--- src/bwapi/__init__.py
+++ src/bwapi/__init__.py
@@ -1,3 +1,11 @@
 """Top-level package for bwapi."""
+from warnings import warn
 
 __version__ = "4.1.0"
+
+warn(
+    "The bwapi package is deprecated. Please use 'bcr-api' instead: "
+    "https://github.com/BrandwatchLtd/bcr-api",
+    DeprecationWarning,
+    stacklevel=2,
+)
